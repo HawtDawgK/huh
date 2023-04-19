b04d6c216b49257743b739ad0707b330b4309708
Bird Up <birdupthemusical@outlook.com>
2023-04-19 13:18:54 +0200

    Refactor TagUtil

--- src/main/java/nsfw/util/TagUtil.java
+++ src/main/java/nsfw/util/TagUtil.java
@@ -1,6 +1,6 @@
 package nsfw.util;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class TagUtil {
@@ -8,14 +8,8 @@ public class TagUtil {
     private static final String[] disallowedTags = {"loli", "shota", "underage", "baby"};
 
     public static List<String> getDisallowedTags(String tags) {
-        List<String> disallowedTagsFound = new ArrayList<>();
-
-        for (String disallowedTag : disallowedTags) {
-            if (tags.contains(disallowedTag)) {
-                disallowedTagsFound.add(disallowedTag);
-            }
-        }
-
-        return disallowedTagsFound;
+        return Arrays.stream(disallowedTags).
+                filter(tags::contains)
+                .toList();
     }
 }
